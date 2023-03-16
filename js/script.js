41aa661244367c919fd3471ca923298344d2ab76
Adrianna Nowak <adriannanowakk@gmail.com>
2023-03-16 13:42:03 +0100

    move const links to the generateTitleLinks function

--- js/script.js
+++ js/script.js
@@ -8,16 +8,14 @@
 
 const titleClickHandler = function(event){
 
-
-
-  console.log('Link was clicked!');
+  // console.log('Link was clicked!');
 
   /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
   for(let activeLink of activeLinks){
    activeLink.classList.remove('active');
 }
-  // /* [IN PROGRESS] add class 'active' to the clicked link */
+  // /* [DONE] add class 'active' to the clicked link */
   event.preventDefault();
   const clickedElement = this;
 
@@ -40,10 +38,56 @@ const titleClickHandler = function(event){
 
   // /* add class 'active' to the correct article */
   targetArticle.classList.add('active');
+  // ----
+
 }
+
+const optArticleSelector = '.post',
+optTitleSelector = '.post-title',
+optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+
+  /* remove contents of titleList */
+ const titleList = document.querySelector(optTitleListSelector);
+ titleList.innerHTML = '';
+
+
+  /* for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  let html = "";
+
+  for (let article of articles){
+
+    /* get the article id */
+    const articleId = article.getAttribute("id");
+
+    /* find the title element */ /* get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    // console.log (linkHTML);
+
+    /* insert link into titleList */
+    // titleList.innerHTML = titleList.innerHTML + linkHTML;
+    // titleList.insertAdjacentHTML("beforeend",linkHTML);
+    html = html + linkHTML;
+    // console.log (html);
+  }
+  titleList.innerHTML = html;
+
   const links = document.querySelectorAll('.titles a');
 
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+
+}
+
+generateTitleLinks();
+
+
 }

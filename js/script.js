da129f75066a11889dd57e099c4542778bef7c59
Adrianna Nowak <adriannanowakk@gmail.com>
2023-03-20 19:00:53 +0100

    add generateTags function to the js file

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles';
 
-  const optArticleTagsSelector = '.post-tags .list';
+
 
   const generateTitleLinks = function generateTitleLinks(){
 
@@ -88,29 +88,40 @@
 }
 
 function generateTags(){
+
+  const optArticleTagsSelector = '.post-tags .list';
+
+
   /* find all articles */
+  const articles = document.querySelectorAll('article');
 
   /* START LOOP: for every article: */
+  for (let article of articles){
 
     /* find tags wrapper */
+    const tagsList = article.querySelector(optArticleTagsSelector);
 
     /* make html variable with empty string */
+    let html = '';
 
     /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
 
     /* split tags into array */
-
+    const articleTagsArray = articleTags.split(' ');
     /* START LOOP: for each tag */
+    for (let tag of articleTagsArray){
 
       /* generate HTML of the link */
-
+      const linkHTML = '<li><a href="#' + tag + '">' + tag + '</a></li>';
+      console.log (linkHTML);
       /* add generated code to html variable */
-
+      html = html +linkHTML;
     /* END LOOP: for each tag */
-
+    }
     /* insert HTML of all the links into the tags wrapper */
-
+    tagsList.innerHTML = html;
   /* END LOOP: for every article: */
-}
+  }}
 
 generateTags();

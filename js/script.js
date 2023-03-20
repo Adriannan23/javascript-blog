6163711de7edb06bf001151b2d4b7d7e35267e53
Adrianna Nowak <adriannanowakk@gmail.com>
2023-03-20 18:04:21 +0100

    remove tags from html code

--- js/script.js
+++ js/script.js
@@ -45,6 +45,8 @@
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles';
 
+  const optArticleTagsSelector = '.post-tags .list';
+
   const generateTitleLinks = function generateTitleLinks(){
 
     /* remove contents of titleList */
@@ -84,3 +86,31 @@
   };
   generateTitleLinks();
 }
+
+function generateTags(){
+  /* find all articles */
+
+  /* START LOOP: for every article: */
+
+    /* find tags wrapper */
+
+    /* make html variable with empty string */
+
+    /* get tags from data-tags attribute */
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+      /* generate HTML of the link */
+
+      /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();

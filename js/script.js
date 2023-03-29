41d582365705ad6a53432aa2a74e526f24d96694
Adrianna Nowak <adriannanowakk@gmail.com>
2023-03-29 13:07:10 +0200

    update generateTags() function with 'new' lines

--- js/script.js
+++ js/script.js
@@ -42,7 +42,8 @@
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles',
     optArticleTagsSelector = '.post-tags .list',
-    optArticleAuthorSelector = '.post-author';
+    optArticleAuthorSelector = '.post-author',
+    optTagsListSelector = '.tags.list';
 
   function generateTitleLinks(customSelector = '') {
 
@@ -85,6 +86,9 @@
 
   function generateTags() {
 
+    /* [NEW] create a new variable allTags with an empty array */
+    let allTags = [];
+
     /* find all articles */
     const articles = document.querySelectorAll('article');
 
@@ -113,12 +117,25 @@
         /* add generated code to html variable */
         html = html + linkHTML + ' ';
 
+        /* [NEW] check if this link is NOT already in allTags */
+        if (allTags.indexOf(linkHTML) == -1) {
+          /* [NEW] add generated code to allTags array */
+          allTags.push(linkHTML);
+        }
+
         /* END LOOP: for each tag */
       }
+
       /* insert HTML of all the links into the tags wrapper */
       tagsWrapper.innerHTML = html;
       /* END LOOP: for every article: */
     }
+
+    /* [NEW] find list of tags in right column */
+    const tagList = document.querySelector(optTagsListSelector);
+
+    /* [NEW] add html from allTags to tagList */
+    tagList.innerHTML = allTags.join(' ');
   }
 
   generateTags();

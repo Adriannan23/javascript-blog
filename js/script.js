eb46c1d6e535df4bd81169c1ba072f489b056fd8
Adrianna Nowak <adriannanowakk@gmail.com>
2023-04-06 23:11:30 +0200

    update generateTags function

--- js/script.js
+++ js/script.js
@@ -83,21 +83,24 @@
   }
   generateTitleLinks();
 
+  // TU SKONCZYLAS
+  // function tagsParams(tags) { }
+  // TU SKONCZYLAS
 
   function generateTags() {
 
-    /* [NEW] create a new variable allTags with an empty array */
-    let allTags = [];
+    /* [NEW] create a new variable allTags with an empty object */
+    let allTags = {};
 
     /* find all articles */
-    const articles = document.querySelectorAll('article');
+    const articles = document.querySelectorAll(optArticleSelector);
 
     /* START LOOP: for every article: */
     for (let article of articles) {
 
       /* find tags wrapper */
       // Miejsce gdzie bdziemy zapisywac wygenerowane tagi? tylko w jednym mijscu bedziemy zapisywac wygenerowane tagi, dlatego jest pojedynczy querySelector.
-      const tagsWrapper = article.querySelector(optArticleTagsSelector);
+      const tagsList = article.querySelector(optArticleTagsSelector);
 
       /* make html variable with empty string */
       let html = '';
@@ -118,24 +121,42 @@
         html = html + linkHTML + ' ';
 
         /* [NEW] check if this link is NOT already in allTags */
-        if (allTags.indexOf(linkHTML) == -1) {
-          /* [NEW] add generated code to allTags array */
-          allTags.push(linkHTML);
+        if (!allTags[tag]) {
+          /* [NEW] add tag to allTags object */
+          allTags[tag] = 1;
+        } else {
+          allTags[tag]++;
         }
 
         /* END LOOP: for each tag */
       }
 
       /* insert HTML of all the links into the tags wrapper */
-      tagsWrapper.innerHTML = html;
+      tagsList.innerHTML = html;
       /* END LOOP: for every article: */
     }
 
     /* [NEW] find list of tags in right column */
     const tagList = document.querySelector(optTagsListSelector);
 
-    /* [NEW] add html from allTags to tagList */
-    tagList.innerHTML = allTags.join(' ');
+    // TUUU
+    // const tagsParams = calculateTagsParams(allTags);
+    // console.log('tagsParams:', tagsParams)
+    // TUUU
+
+    /* [NEW] create variable for all links HTML code */
+    let allTagsHTML = '';
+
+    /* [NEW] START LOOP: for each tag in allTags: */
+    for (let tag in allTags) {
+      /* [NEW] generate code of a link and add it to allTagsHTML */
+      allTagsHTML += tag + ' (' + allTags[tag] + ') ';
+    }
+    /* [NEW] END LOOP: for each tag in allTags: */
+
+    /*[NEW] add HTML from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML;
+
   }
 
   generateTags();

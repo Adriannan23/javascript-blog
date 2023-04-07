c6069bdcadc1367ba69d90b6d15ef1dff3675096
Adrianna Nowak <adriannanowakk@gmail.com>
2023-04-07 11:51:03 +0200

    add links with their count to the TAGS

--- js/script.js
+++ js/script.js
@@ -150,7 +150,8 @@
     /* [NEW] START LOOP: for each tag in allTags: */
     for (let tag in allTags) {
       /* [NEW] generate code of a link and add it to allTagsHTML */
-      allTagsHTML += tag + ' (' + allTags[tag] + ') ';
+      allTagsHTML += '<li><a href="#tag-' + tag + '">' + tag + ' ' + '(' + allTags[tag] + ')' + '</a></li>';
+
     }
     /* [NEW] END LOOP: for each tag in allTags: */
 
